15797847bc0b97fda8b60645d83198339d217fb9
Richard Potter <richardpotter.au@gmail.com>
2023-07-26 15:31:50 +1000

    wrap in current user provider

--- src/App.js
+++ src/App.js
@@ -5,14 +5,17 @@ import CommentThreadList from "./components/CommentThreadList";
 import { getCurrentUser, getCommentThreads } from "./data/dummyData";
 
 import classes from "./App.module.css";
+import CurrentUserProvider from "./context/current-user-context";
 
-const currentUser = getCurrentUser();
+//const currentUser = getCurrentUser();
 const dummyCommentThreads = getCommentThreads();
 
 function App() {
   return (
     <main className={classes.main}>
-      <CommentThreadList commentThreads={dummyCommentThreads} />
+      <CurrentUserProvider>
+        <CommentThreadList commentThreads={dummyCommentThreads} />
+      </CurrentUserProvider>
     </main>
   );
 }
