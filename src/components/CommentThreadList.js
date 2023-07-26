a198c5117f5993540a1b5356e2b517cda41cb733
Richard Potter <richardpotter.au@gmail.com>
2023-07-26 10:15:50 +1000

    import css

--- src/components/CommentThreadList.js
+++ src/components/CommentThreadList.js
@@ -2,9 +2,11 @@ import React from "react";
 
 import CommentThread from "./CommentThread";
 
+import classes from "./CommentThreadList.module.css";
+
 function CommentThreadList({ commentThreads }) {
   return (
-    <ul>
+    <ul className={classes.threadListContainer}>
       {commentThreads.map((commentThread) => {
         return <CommentThread commentThread={commentThread} />;
       })}
