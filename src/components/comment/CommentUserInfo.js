63860eb828ec2f2eea802af838603e54c6d73eea
Richard Potter <richardpotter.au@gmail.com>
2023-07-26 15:00:37 +1000

    style user info

--- src/components/comment/CommentUserInfo.js
+++ src/components/comment/CommentUserInfo.js
@@ -1,5 +1,7 @@
 import React from "react";
 
+import classes from "./CommentUserInfo.module.css";
+
 function getAvatarPath(username) {
   return `/images/avatars/image-${username}.png`;
 }
@@ -7,14 +9,15 @@ function getAvatarPath(username) {
 function CommentUserInfo({ user }) {
   console.log(process.env.PUBLIC_URL + getAvatarPath(user.username));
   return (
-    <>
+    <div className={classes.container}>
       <img
+        className={classes.image}
         src={process.env.PUBLIC_URL + getAvatarPath(user.username)}
         alt={`user ${user.userName} avatar`}
       />
       <p>{user.username}</p>
       <p>YOU</p>
-    </>
+    </div>
   );
 }
 
