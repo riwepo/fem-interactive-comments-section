26c29885b2d78579fcf6282213590ab3bb188cc5
Richard Potter <richardpotter.au@gmail.com>
2023-07-28 08:35:23 +1000

    remove dead code

--- src/App.js
+++ src/App.js
@@ -2,13 +2,12 @@ import React from "react";
 
 import CommentThreadList from "./components/CommentThreadList";
 
-import { getCurrentUser, getCommentThreads } from "./data/dummyData";
+import { getCommentThreads } from "./data/dummyData";
 
 import classes from "./App.module.css";
 import CurrentUserProvider from "./context/current-user-context";
 import NewCommentEditor from "./components/NewCommentEditor";
 
-//const currentUser = getCurrentUser();
 const dummyCommentThreads = getCommentThreads();
 
 function App() {
