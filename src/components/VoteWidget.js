cf7ba6ac66693f30fe2554ee87c3e2bf9374556f
Richard Potter <richardpotter.au@gmail.com>
2023-07-27 11:35:10 +1000

    implement voting widget styling

--- src/components/VoteWidget.js
+++ src/components/VoteWidget.js
@@ -1,7 +1,22 @@
 import React from "react";
 
-function VoteWidget() {
-  return <h1>vote widget</h1>;
+import iconPlus from "../images/icon-plus.svg";
+import iconMinus from "../images/icon-minus.svg";
+
+import classes from "./VoteWidget.module.css";
+
+function VoteWidget({ count, onPlusClick, onMinusClick }) {
+  return (
+    <div className={classes.container}>
+      <button className={classes.button} onClick={onPlusClick}>
+        <img className={classes.buttonImg} src={iconPlus} alt="plus icon" />
+      </button>
+      <p className={classes.count}>{count}</p>
+      <button className={classes.button} onClick={onMinusClick}>
+        <img className={classes.buttonImg} src={iconMinus} alt="minus icon" />
+      </button>
+    </div>
+  );
 }
 
 export default VoteWidget;
