626bda9b53864f5b624df475fe3e5d41cc5a1773
Richard Potter <richardpotter.au@gmail.com>
2023-07-27 15:13:55 +1000

    style action buttons with hover effects

--- src/components/comment/CommmentActions.js
+++ src/components/comment/CommmentActions.js
@@ -1,7 +1,8 @@
 import React from "react";
-import iconReply from "../../images/icon-reply.svg";
-import iconDelete from "../../images/icon-delete.svg";
-import iconEdit from "../../images/icon-edit.svg";
+
+import { ReactComponent as IconReply } from "../../images/icon-reply.svg";
+import { ReactComponent as IconDelete } from "../../images/icon-delete.svg";
+import { ReactComponent as IconEdit } from "../../images/icon-edit.svg";
 
 import classes from "./CommentActions.module.css";
 
@@ -10,18 +11,18 @@ function CommentActions({ className, isYou }) {
     <div className={`${className} ${classes.container}`}>
       {!isYou && (
         <button className={`${classes.button} ${classes.buttonReply}`}>
-          <img src={iconReply} alt="reply icon" />
+          <IconReply className={classes.iconReply} />
           <p>Reply</p>
         </button>
       )}
       {isYou && (
         <div className={classes.buttonContainer}>
           <button className={`${classes.button} ${classes.buttonDelete}`}>
-            <img src={iconDelete} alt="delete icon" />
+            <IconDelete className={classes.iconDelete} />
             <p>Delete</p>
           </button>
           <button className={`${classes.button} ${classes.buttonEdit}`}>
-            <img src={iconEdit} alt="edit icon" />
+            <IconEdit className={classes.iconEdit} />
             <p>Edit</p>
           </button>
         </div>
