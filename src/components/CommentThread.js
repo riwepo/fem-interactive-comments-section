b171f4d83fbf4c62d2ed59c6551de577ed81e4b9
Richard Potter <richardpotter.au@gmail.com>
2023-07-28 10:48:52 +1000

    standardize gaps between cards add debug borders add more comment editors

--- src/components/CommentThread.js
+++ src/components/CommentThread.js
@@ -1,11 +1,14 @@
 import React from "react";
+
 import Replies from "./Replies";
 import Comment from "./comment/Comment";
+import NewCommentEditor from "./NewCommentEditor";
 
 function CommentThread({ commentThread }) {
   return (
     <li>
       <Comment comment={commentThread} />
+      <NewCommentEditor />
       {commentThread.replies && <Replies comments={commentThread.replies} />}
     </li>
   );
