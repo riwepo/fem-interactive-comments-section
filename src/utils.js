77bdeaaddab2283fd97ae0f43791c36e869f2de6
Richard Potter <richardpotter.au@gmail.com>
2023-07-28 15:05:31 +1000

    added method to transform flat data to threads

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,29 @@
 export function getAvatarPath(username) {
   return `/images/avatars/image-${username}.png`;
 }
+
+// transform flat comments data to threads
+export function transformCommentsToThreads(comments) {
+  const topLevelComments = comments.filter((c) => !c.replyingTo);
+  const topLevelTransformed = topLevelComments.map(transform);
+  const withReplies = topLevelTransformed.map((x) => {
+    const replies = comments.filter((y) => y.replyingTo === x.id);
+    const transformedReplies = replies.map(transform);
+    const result = { ...x, replies: transformedReplies };
+    return result;
+  });
+  return withReplies;
+}
+
+// turn user into object
+// add empty replies array
+function transform(comment) {
+  return {
+    id: comment.id,
+    content: comment.content,
+    createdAt: comment.createdAt,
+    score: comment.score,
+    user: { username: comment.username },
+    replies: [],
+  };
+}
