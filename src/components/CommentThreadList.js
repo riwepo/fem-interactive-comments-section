4c660cca4b2e30e5778b779da29029f6bd15f33c
Richard Potter <richardpotter.au@gmail.com>
2023-07-28 15:03:45 +1000

    get comments from context

--- src/components/CommentThreadList.js
+++ src/components/CommentThreadList.js
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import CommentThread from "./CommentThread";
 
+import { CommentsContext } from "../context/comments-context";
+
 import classes from "./CommentThreadList.module.css";
 
-function CommentThreadList({ commentThreads }) {
+function CommentThreadList() {
+  const commentsContext = useContext(CommentsContext);
+  const commentThreads = commentsContext.getCommentThreads();
   return (
     <ul className={classes.threadListContainer}>
       {commentThreads.map((commentThread) => {
