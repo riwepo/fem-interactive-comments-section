e32b0078f08098f5645c200d1f2cbc41248e2baf
Richard Potter <richardpotter.au@gmail.com>
2023-07-29 11:25:19 +1000

    change structure of reply separate reply comment id from reply username

--- src/utils.js
+++ src/utils.js
@@ -6,38 +6,22 @@ export function getAvatarPath(username) {
 export function transformCommentsToThreads(comments) {
   const topLevelComments = comments.filter((c) => !c.replyToCommentId);
   // console.log("topLevelComments", topLevelComments);
-  const topLevelTransformed = topLevelComments.map((x) =>
-    transform(comments, x)
-  );
+  const topLevelTransformed = topLevelComments.map(transform);
   const withReplies = topLevelTransformed.map((x) => {
     const replies = comments.filter((y) => y.replyToCommentId === x.id);
-    const transformedReplies = replies.map((x) => transform(comments, x));
+    const transformedReplies = replies.map(transform);
     const result = { ...x, replies: transformedReplies };
     return result;
   });
-  // console.log("withReplies", withReplies);
   return withReplies;
 }
 
-// add the replyToUsername, found from the replyToCommentId
 // turn user into object
 // add empty replies array
-function transform(comments, comment) {
-  const withReplyToUsername = addReplyToUsername(comments, comment);
+function transform(comment) {
   return {
-    ...withReplyToUsername,
+    ...comment,
     user: { username: comment.username },
     replies: [],
   };
-}
-
-function addReplyToUsername(comments, comment) {
-  let replyToUsername = null;
-  if (comment.replyToCommentId) {
-    const repliedComment = comments.find(
-      (x) => x.id === comment.replyToCommentId
-    );
-    replyToUsername = repliedComment.username;
-  }
-  return { ...comment, replyToUsername };
 }
