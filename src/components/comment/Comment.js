3c6f5bb60e1ec504d9482085d754089d3e5c92fe
Richard Potter <richardpotter.au@gmail.com>
2023-07-31 09:29:29 +1000

    allow editing of comment

--- src/components/comment/Comment.js
+++ src/components/comment/Comment.js
@@ -5,12 +5,14 @@ import CommentUserInfo from "./CommentUserInfo";
 import CommentVote from "./CommentVote";
 import CommentActions from "./CommmentActions";
 import Modals from "../modal/Modals";
+import Button from "../ui/Button";
 import { CurrentUserContext } from "../../context/current-user-context";
 
 import classes from "./Comment.module.css";
 
 function Comment({ comment, onCreateReplyClick }) {
   const [isRequestingDelete, setIsRequestingDelete] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
   const currentUser = useContext(CurrentUserContext);
   const userIsCurrentUser = currentUser.username === comment.user.username;
   const confirmDeleteHandler = (confirm) => {
@@ -24,7 +26,9 @@ function Comment({ comment, onCreateReplyClick }) {
   const requestDeleteClickHandler = () => {
     setIsRequestingDelete(true);
   };
-  const editClickHandler = () => {};
+  const editClickHandler = () => {
+    setIsEditing((current) => !current);
+  };
 
   return (
     <>
@@ -37,27 +41,42 @@ function Comment({ comment, onCreateReplyClick }) {
             user={comment.user}
           />
           <p className={classes.createdAt}>{comment.createdAt}</p>
-          <p className={classes.content}>
-            {comment.replyToUsername && (
-              <span
-                className={classes.replyName}
-              >{`@${comment.replyToUsername} `}</span>
-            )}
-            {comment.content}
-          </p>
+          {!isEditing && (
+            <p className={classes.content}>
+              {comment.replyToUsername && (
+                <span
+                  className={classes.replyName}
+                >{`@${comment.replyToUsername} `}</span>
+              )}
+              {comment.content}
+            </p>
+          )}
+          {isEditing && (
+            <div className={classes.editingContainer}>
+              <textarea className={classes.textarea}>
+                {comment.content}
+              </textarea>
+              <Button className={classes.button} disabled={true} onClick={null}>
+                UPDATE
+              </Button>
+            </div>
+          )}
+
           <CommentVote
             className={classes.vote}
             count={comment.score}
             onPlusClick={votePlusClickHandler}
             onMinusClick={voteMinusClickHandler}
           />
-          <CommentActions
-            className={classes.actions}
-            isYou={userIsCurrentUser}
-            onCreateReplyClick={onCreateReplyClick}
-            onRequestDeleteClick={requestDeleteClickHandler}
-            onEditClick={editClickHandler}
-          />
+          {!isEditing && (
+            <CommentActions
+              className={classes.actions}
+              isYou={userIsCurrentUser}
+              onCreateReplyClick={onCreateReplyClick}
+              onRequestDeleteClick={requestDeleteClickHandler}
+              onEditClick={editClickHandler}
+            />
+          )}
         </div>
       </Card>
     </>
