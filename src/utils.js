2fee55063409a83244eb42e13d671ca801ebf99e
Richard Potter <richardpotter.au@gmail.com>
2023-07-31 10:36:35 +1000

    sort replies by id

--- src/utils.js
+++ src/utils.js
@@ -5,12 +5,15 @@ export function getAvatarPath(username) {
 // transform flat comments data to threads
 export function transformCommentsToThreads(comments) {
   const topLevelComments = comments.filter((c) => !c.replyToCommentId);
+  // highest score first
   const sortedTopLevel = topLevelComments.sort((a, b) => b.score - a.score);
   // console.log("sortedTopLevel", sortedTopLevel);
   const topLevelTransformed = sortedTopLevel.map(transform);
   const withReplies = topLevelTransformed.map((x) => {
     const replies = comments.filter((y) => y.replyToCommentId === x.id);
-    const transformedReplies = replies.map(transform);
+    // lowest id first
+    const sortedReplies = replies.sort((a, b) => a.score - b.score);
+    const transformedReplies = sortedReplies.map(transform);
     const result = { ...x, replies: transformedReplies };
     return result;
   });
